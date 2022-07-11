837e3480085b7f7cf0f38070953ebe71723efd1d
pooja1609 <108878487+pooja1609@users.noreply.github.com>
2022-07-11 21:21:48 +0530

    Add files via upload

---
+++ ShoppingApplication/digital-shopping/shopping/src/main/java/com/adapty/shopping/services/CartImpl.java
@@ -0,0 +1,75 @@
+package com.adapty.shopping.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.adapty.shopping.entities.Cart;
+import com.adapty.shopping.entities.Product;
+import com.adapty.shopping.entities.STATUS;
+import com.adapty.shopping.repositories.CartRepository;
+//import com.adapty.shopping.repositories.ProductRepository;
+import com.adapty.shopping.repositories.ProductRepository;
+
+@Service
+public class CartImpl implements CartInterface {
+    @Autowired
+    private ProductRepository repoObj;
+    
+    @Autowired
+    private CartRepository cartRepo;
+
+    public List<Cart> findAllCarts(){
+        return cartRepo.findAll();
+    }
+
+    public Optional<Cart> findByCartId(String cartId){
+        return cartRepo.findById(cartId);
+    }
+
+    public String addToCart(Cart cartObj){
+    
+        Optional<Product> obj = repoObj.findById(cartObj.getProductId());
+        if(obj.isPresent()){
+        if(obj.get().getProductStatus()==STATUS.ACTIVE){
+            cartRepo.save(cartObj);
+            return "Added Successfully";
+        }
+        else{
+            return "Product no longer exist";
+    }
+        }else{
+            return "Does not exist";
+        }
+        
+}
+
+    public String deleteByCartItemId(String cartItemId){
+        cartRepo.deleteById(cartItemId);
+        return "object deleted";
+    }
+
+    public Cart updateByProductId(Cart cartObj){
+        if(cartObj.getCartItemId()== null){
+            return null;
+        }
+        else{
+            Optional<Cart> c1 = cartRepo.findById(cartObj.getCartItemId());
+            c1.get().setCartItemQty(cartObj.getCartItemQty());
+            c1.get().setProductId(cartObj.getProductId());
+            cartRepo.deleteById(cartObj.getCartItemId());
+            return cartRepo.save(cartObj);
+        }
+
+    }
+
+    public String deleteByProductId(String productId){
+        cartRepo.deleteByProductId(productId);
+        
+        return "Deleted";
+}
+
+
+}
